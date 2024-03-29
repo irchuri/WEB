f93b897e97944f9e4a9f85236dcf4bfa456dbfe5
irchuri <d4riakert@ya.ru>
2024-03-29 22:38:06 +0200

    added task 2a code

--- lab3/code/index.php
+++ lab3/code/index.php
@@ -1,7 +1,26 @@
 <?php
-$str = 'a1b2c3';
-$new_str = preg_replace_callback('/\d+/', function($matches){
-    return pow($matches[0],3);
-}, $str);
+if(isset($_POST['submit'])){
+    $text = $_POST['your_text'];
+    $wordCount = countWords($text);
+    $charCount = mb_strlen($text, 'UTF-8');
+    echo "Количество слов: $wordCount<br>";
+    echo "Количество символов: $charCount";
+}
 
-echo $new_str;
+function countWords($text) {
+    $charlist = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyz';
+    return str_word_count(mb_strtolower($text, 'UTF-8'), 0, $charlist);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+<body>
+<form method="post">
+    <textarea name="your_text"></textarea><br>
+    <input type="submit" name="submit" value="Count">
+</form>
+</body>
+</html>

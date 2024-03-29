78610185f4fd068ed65a2717d23fef235143bc15
irchuri <d4riakert@ya.ru>
2024-03-29 22:50:20 +0200

    added solution to task2b.php

--- lab3/code/task2b.php
+++ lab3/code/task2b.php
@@ -1,25 +1,19 @@
 <?php
 session_start();
 
-// Обработка POST-запроса
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Проверяем и сохраняем фамилию, если она была отправлена
     if (!empty($_POST['surname'])) {
         $_SESSION['surname'] = $_POST['surname'];
     }
-
-    // Проверяем и сохраняем имя, если оно было отправлено
     if (!empty($_POST['name'])) {
         $_SESSION['name'] = $_POST['name'];
     }
-
-    // Проверяем и сохраняем возраст, если он был отправлен
     if (!empty($_POST['age'])) {
         $_SESSION['age'] = $_POST['age'];
     }
 }
 
-// Выводим данные, если они есть в сессии
 if (isset($_SESSION['surname'], $_SESSION['name'], $_SESSION['age'])) {
     echo 'Surname: ' . $_SESSION['surname'] . '<br>';
     echo 'Name: ' . $_SESSION['name'] . '<br>';
